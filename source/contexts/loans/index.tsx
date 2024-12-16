5dc1cd56daea111f891de451c6b4a9da87c11286
Konstantin Terziev <kd.terziev@gmail.com>
2024-12-16 06:03:29 +0000

    remove hardcoded data

--- source/contexts/loans/index.tsx
+++ source/contexts/loans/index.tsx
@@ -21,32 +21,11 @@ type LoanAction =
 	| {type: 'added'; loan: Omit<Loan, 'id'>}
 	| {type: 'changed'; loan: Loan};
 
-const initialLoans = [
-	{
-		id: uuidv4(),
-		startDate: '2021-09-11',
-		endDate: '2022-09-12',
-		amount: 1234,
-		currency: 'GBP',
-		baseInterestRate: 7,
-		margin: 2,
-	},
-	{
-		id: uuidv4(),
-		startDate: '2023-09-27',
-		endDate: '2026-09-02',
-		amount: 1234,
-		currency: 'EUR',
-		baseInterestRate: 8,
-		margin: 3,
-	},
-];
-
 const LoansContext = createContext<Loan[]>([]);
 const LoansDispatchContext = createContext({} as Dispatch<LoanAction>);
 
 export function LoansProvider({children}: PropsWithChildren) {
-	const [loans, dispatch] = useReducer(loansReducer, initialLoans);
+	const [loans, dispatch] = useReducer(loansReducer, []);
 
 	return (
 		<LoansContext.Provider value={loans}>
